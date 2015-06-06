4828ace7de11eaee331baf13ab3c5884bbb84283
David Begin <davidmichaelbe@gmail.com>
2015-06-05 19:25:37 -0700

    got some lifetimes but problems to use them for yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#![allow(dead_code)]
+
 fn main() {
     println!("Messin with Lifetimes");
+
+    let y = &"durf"; // this is the same as `let _y = 5; let y = &_y;`
+
+    let strukt = Strukt { name: y };
+
+    println!("{}", strukt.name);
+
+    bar();
+    foo();
+}
+
+fn bar<'a_lifetime>() {
+    println!("This has a lifetime");
+}
+
+fn foo<'another_lifetime>() {
+    println!("This also has a lifetime");
+}
+
+struct Strukt<'crazy_lifetime> {
+  name: &'crazy_lifetime str,
 }
