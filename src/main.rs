405f1f9fc5f8f87529ff439d8977262245a2d1b2
David Begin <davidmichaelbe@gmail.com>
2015-06-07 16:59:50 -0700

    drop for introspective

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,39 @@
-#![allow(dead_code)]
-
-struct Foo<'a> {
-    x: &'a i32,
-}
+#![allow(dead_code, unused_variables)]
 
 fn main() {
-    let x;
-                              //  |
-    {                         //  |
-        let y = &5;           // ---+ y goes into scope
-        let f = Foo { x: y }; // ---+ f goes into scope
-        x = &f.x;             //  | | error here
-    }                         // ---+ f and y go out of scope
-                              //  |
-    println!("{}", x);        //  |
+    // The Problem:
+    //      * I acquire a handle to some kind of resource.
+    //      * I lend you a reference to the resource.
+    //      * I decide I’m done with the resource, and deallocate it,
+    //          while you still have your reference.
+    //      * You decide to use the resource.
+
+    foo2();
 }
 
-// fn main() {
-//     let x;
-//
-//     {
-//         x = &"woah";
-//         let f = Strukt { name: x };
-//         x = &f.name;
-//     }
-//
-//     println!("name: {}", x);
-// }
-//
-// struct Strukt<'crazy_lifetime> {
-//   name: &'crazy_lifetime str,
-// }
-//
-// struct Foo<'a> {
-//     x: &'a i32,
-// }
+fn foo() {
+    let v = vec![1, 2, 3];
+}
 
-// fn main() {
-//     let x;                    // -+ x goes into scope
-//                               //  |
-//     {                         //  |
-//         let y = &5;           // ---+ y goes into scope
-//         let f = Foo { x: y }; // ---+ f goes into scope
-//         x = &f.x;             //  | | error here
-//     }                         // ---+ f and y go out of scope
-//                               //  |
-//     println!("{}", x);        //  |
-// }                             // -+ x goes out of scope
+// when foo is called, v is allocated on the heap
+// then at the end of foo,
+// v goes out of scope
+// and is deallocated from the heap
+
+// let use drop to see this
+fn foo2() {
+    // println!("v is being bound to a new Person struct");
+    println!("the start of foo2");
+    let v = Person { age: 32 };
+    println!("the end of foo2")
+}
+
+struct Person {
+    age: i32
+}
+
+impl Drop for Person {
+    fn drop(&mut self) {
+        println!("dropping Person {} with age: ", self.age);
+    }
+}
